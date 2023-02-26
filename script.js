0a1cea3e9449f238c23299d8e0fe660373dc75db
edinson.rey4933gmail.com <edinson.rey4933@gmail.com>
2023-02-26 18:39:38 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function ventana() {
+  let dato= document.getElementById("date").value;
+  let fecha=new Date (dato);
+
+
+  document.getElementById("res").innerHTML = "Hola tu mes de cumpleaños es <b>DICIMBRE</b> por lo tanto tus periodos durante esta vigencia se dividen asi:<br><br><b><center>PERIODO DISPONIBLE</center></b><br><br>Comprende los meses de <b>ENERO FEBRERO Y MARZO</b><br><br>Solo debes estar atento a cualquier requerimiento de entrenamiento por parte del personal Instructor en caso que se requiera.<br><br><b><center>PERIODO EDSA</center></b><br><br>Comprende los meses de <b>ABRIL MAYO Y JUNIO</b><br><br> En este trimestre un instructor debera realizarte el Examen de proeficiencia sin previo aviso, asi que debes repasar normatividad y manual del equipo, en cualquier momento durante este periodo un istructor tomara contacto.<br><br><b><center>PERIODO REPASO</center></b><br><br>Comprende los meses de <b>JULIO AGOSTO Y SEPTIEMBRE</b><br><br> Es momento de prepararse estudiando normatividad y manual del equipo SIART.<br><br><b><center>PERIODO EDPA</center></b><br><br>Comprende los meses de <b>OCTUBRE NOVIEMBRE AGOSTO Y DICIEMBRE</b><br><br> Es momento de evaluar tu proeficiencia por parte de un isntructor, y realizar las siguientes actividades:<br>Examen Manual del Operador y de limites<br>CRM<br>Vuelo de estandarizacion<br>Renovar licencia medica."
+  
+  ;
+  }
